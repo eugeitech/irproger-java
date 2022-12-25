b85b6d65a11f31afacd8b421ab7d9e5ad3666d46
Euge <euge.itech@gmail.com>
2022-12-25 19:27:01 +0100

    #5  Данные от пользователя. Математические деиствия

---
+++ src/main/java/com/itproger/scanner/MainScanner.java
@@ -0,0 +1,19 @@
+package com.itproger.scanner;
+
+import java.util.Scanner;
+
+public class MainScanner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write your name: ");
+        String userName = scanner.nextLine();
+        System.out.println("Hi, " + userName);
+
+        int num1 = scanner.nextInt();
+        byte num2 = scanner.nextByte();
+        boolean b = scanner.nextBoolean();
+        float num3 = scanner.nextFloat();
+    }
+}
---
+++ src/main/java/com/itproger/string/MainString.java
@@ -0,0 +1,18 @@
+package com.itproger.string;
+
+public class MainString {
+
+    public static void main(String[] args) {
+        // Comment
+        System.out.println("Hello\\ \"\n\tWorld!");
+
+        /*
+        Comment
+         */
+
+        int age = -5;
+        System.out.println("Age: " + age + ".");
+        age = 5;
+        System.out.println(age);
+    }
+}
