bc7cbf7851dfff78cd7b9ea8c2ef39b1d1ce25e3
Euge <euge.itech@gmail.com>
2023-01-11 16:06:18 +0100

    #9  Коллекции в Джава (Collections Framework)

---
+++ src/main/java/com/itproger/collections/Linked.java
@@ -0,0 +1,18 @@
+package com.itproger.collections;
+
+import java.util.LinkedList;
+
+public class Linked {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> numbers = new LinkedList<>();
+        numbers.add(5);
+        numbers.add(6);
+
+        //remove, get, clear
+
+        for(Integer element : numbers)
+            System.out.println(element);
+    }
+}
