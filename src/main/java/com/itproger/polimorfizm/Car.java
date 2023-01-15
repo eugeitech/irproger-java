fdc6ab2bbce890e3feae9bd610b42fffbdeee752
Euge <euge.itech@gmail.com>
2023-01-15 15:56:34 +0100

    #15  Полиморфизм в ООП

---
+++ src/main/java/com/itproger/polimorfizm/Car.java
@@ -0,0 +1,9 @@
+package com.itproger.polimorfizm;
+
+public class Car extends Transport {
+
+    public Car(float speed, int weight, String color, byte[] coordinate) {
+        super(speed, weight, color, coordinate);
+
+    }
+}
