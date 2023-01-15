3289c6687b276ebc2c2907739640f3755669a6d8
Euge <euge.itech@gmail.com>
2023-01-15 14:33:40 +0100

    #14  Наследование в классах

---
+++ src/main/java/com/itproger/inhetirance/Car.java
@@ -0,0 +1,9 @@
+package com.itproger.inhetirance;
+
+public class Car extends Transport {
+
+    public Car(float speed, int weight, String color, byte[] coordinate) {
+        super(speed, weight, color, coordinate);
+
+    }
+}
---
+++ src/main/java/com/itproger/inhetirance/Transport.java
@@ -0,0 +1,42 @@
+package com.itproger.inhetirance;
+
+public class Transport {
+
+    private float speed;
+    private int weight;
+    private String color;
+    private byte[] coordinate;
+
+    public Transport(float speed, int weight, String color, byte[] coordinate) {
+        System.out.println("Object is created");
+        setValues(speed, weight, color, coordinate);
+        System.out.println(getValues());
+    }
+
+    public Transport(int weight, byte[] coordinate) {
+        this.weight = weight;
+        this.coordinate = coordinate;
+        System.out.println(getValues());
+    }
+
+    protected void setValues(float speed, int weight, String color, byte[] coordinate) {
+        this.speed = speed;
+        this.weight = weight;
+        this.color = color;
+        this.coordinate = coordinate;
+    }
+
+    protected String getValues() {
+        String info = "Oject speed: " + speed +
+                "\nWeight: " + weight +
+                "\nColor: " + color;
+        String infoCoordinate = "\nCoordinate:\n";
+
+        for(int i = 0; i < coordinate.length; i++) {
+            infoCoordinate += coordinate[i] + ", ";
+        }
+        String finalInfoCoordinate = infoCoordinate.substring(0, infoCoordinate.length()-2) + "\n";
+
+        return info + finalInfoCoordinate;
+    }
+}
