5012fbe7c41bdc7ba0200bbc27ae53cb78f19aa8
Euge <euge.itech@gmail.com>
2023-01-16 11:41:55 +0100

    #17  Вложенные и анонимные классы

---
+++ src/main/java/com/itproger/inner/Car.java
@@ -0,0 +1,22 @@
+package com.itproger.inner;
+
+public class Car extends Transport {
+
+    public Engine engine = new Engine();
+
+    public Car(float speed, int weight, String color, byte[] coordinate) {
+        super(speed, weight, color, coordinate);
+
+    }
+
+    @Override
+    public void moveObject(float speed) {
+        System.out.println("Out object move with speed: " + speed);
+    }
+
+    @Override
+    public boolean stopObject() {
+        this.speed = 0;
+        return true;
+    }
+}
---
+++ src/main/java/com/itproger/inner/Main.java
@@ -0,0 +1,29 @@
+package com.itproger.inner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Car bmw = new Car(250.5f, 2500, "White", new byte[] {0, 0, 0});
+//        bmw.engine.setValues(false, 2000);
+//        bmw.engine.info();
+//
+//        Truck truck = new Truck(5600, new byte[] {100, 0, 100});
+//        truck.engine.setValues(true, 10);
+//        truck.engine.info();
+
+//        truck.setValues(250.5f, 2500, "White", new byte[] {0, 0, 0}, true);
+//        System.out.println(truck.getValues());
+
+        Car flyCar = new Car(250.5f, 2500, "White", new byte[] {0, 0, 0}) {
+            @Override
+            public void moveObject(float speed) {
+                super.moveObject(speed);
+
+                this.engine.isReady(true);
+                System.out.println("Car is flying");
+            }
+        };
+
+        flyCar.moveObject(450);
+    }
+}
